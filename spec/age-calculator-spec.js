c0747459183f9702008bc48d960d49d5b04671da
Kira Roberts <rosehill1788@protonmail.com>
2019-09-08 12:06:48 -0700

    add test for AgeOnPlanet object prototype for finding age on Mercury

---
+++ spec/age-calculator-spec.js
@@ -0,0 +1,7 @@
+describe('AgeOnPlanet', function() {
+
+  it('should test whether the AgeOnPlanet class contains the key value pair of Mercury = .24', function() {
+    let age = new AgeOnPlanet(30);
+    expect(AgeOnPlanet.mercury).toEqual()
+  });
+});
